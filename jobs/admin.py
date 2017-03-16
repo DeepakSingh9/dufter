baeca4437965d41a16f918c6d47255a23ba24ed0
DeepakSingh9 <indeepak9@gmail.com>
2017-03-16 23:00:32 +0530

    dufter first commit

---
+++ jobs/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import JobCategory,Job
+
+admin.site.register(Job)
+admin.site.register(JobCategory)
+
+# Register your models here.
---
+++ jobs/models.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+# Create your models here.
+class JobCategory(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(JobCategory, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    position = models.CharField(max_length=128)
+    organisation = models.CharField(max_length=128)
+    keyskills = models.CharField(max_length=128)
+    location = models.CharField(max_length=128)
+    experience = models.CharField(max_length=128)
+    summary = models.TextField(max_length=500)
+    dateposted = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    jobcategory = models.ForeignKey(JobCategory)
+
+    def __str__(self):
+        return self.position, self.organisation
+
+
+
